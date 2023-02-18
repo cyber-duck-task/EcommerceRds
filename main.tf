761f13054007391f42b6b69890c8b07a6c88b4e9
haytest <123085335+haytest@users.noreply.github.com>
2023-02-18 16:42:03 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,76 @@
+provider "aws" {
+  region = var.region
+}
+
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.77.0"
+
+  name                 = "ecommerce"
+  cidr                 = "10.0.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+
+resource "aws_db_subnet_group" "ecommerce" {
+  
+  subnet_ids = module.vpc.public_subnets
+
+  tags = {
+    Name = "Ecommerce"
+  }
+}
+
+resource "aws_security_group" "rds" {
+  name   = "ecommerce_rds"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ecommerce_rds"
+  }
+}
+
+resource "aws_db_parameter_group" "ecommerce" {
+  name   = "ecommerce"
+  family = "mysql8.0"
+
+  parameter {
+    name  = "character_set_server"
+    value = "utf8"
+  }
+}
+
+resource "aws_db_instance" "ecommerce" {
+  identifier             = "ecommerce"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 20
+  storage_type           = "gp2"
+  engine                 = "mysql"
+  engine_version         = "8.0.28"
+  multi_az               = true
+  username               = "admin"
+  password               = var.db_password
+  db_subnet_group_name   = aws_db_subnet_group.ecommerce.name
+  vpc_security_group_ids = [aws_security_group.rds.id]
+  parameter_group_name   = aws_db_parameter_group.ecommerce.name
+  publicly_accessible    = true
+  skip_final_snapshot    = true
+}
