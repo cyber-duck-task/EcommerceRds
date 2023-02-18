2fa02d35c2547c3edfb18489a4b75ddcf457f1aa
haytest <123085335+haytest@users.noreply.github.com>
2023-02-18 18:06:53 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,14 +11,14 @@ module "vpc" {
   name                 = "ecommerce"
   cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
-  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  private_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   enable_dns_hostnames = true
   enable_dns_support   = true
 }
 
 resource "aws_db_subnet_group" "ecommerce" {
   
-  subnet_ids = module.vpc.public_subnets
+  subnet_ids = module.vpc.private_subnets
 
   tags = {
     Name = "Ecommerce"
@@ -71,6 +71,6 @@ resource "aws_db_instance" "ecommerce" {
   db_subnet_group_name   = aws_db_subnet_group.ecommerce.name
   vpc_security_group_ids = [aws_security_group.rds.id]
   parameter_group_name   = aws_db_parameter_group.ecommerce.name
-  publicly_accessible    = true
+  publicly_accessible    = false
   skip_final_snapshot    = true
 }
