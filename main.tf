8deb3809a018f27c449bbd81e22b2326bb72e53d
haytest <123085335+haytest@users.noreply.github.com>
2023-02-19 16:54:33 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,69 +8,69 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.77.0"
 
-  name                 = "ecommerce"
-  cidr                 = "10.0.0.0/16"
+  name                 = var.vpc_name
+  cidr                 = var.cidr
   azs                  = data.aws_availability_zones.available.names
   private_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+  enable_dns_hostnames = var.enable_dns_hostnames
+  enable_dns_support   = var.enable_dns_support
 }
 
 resource "aws_db_subnet_group" "ecommerce" {
   
   subnet_ids = module.vpc.private_subnets
 
-  tags = {
-    Name = "Ecommerce"
-  }
+  tags = var.vpc_tag_name
+
 }
 
 resource "aws_security_group" "rds" {
-  name   = "ecommerce_rds"
+  name   = var.aws_security_group_name
   vpc_id = module.vpc.vpc_id
 
   ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
+    from_port   = var.ingress_from_port
+    to_port     = var.ingress_from_port
+    protocol    = var.ingress_protocol
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
+    from_port   = var.egress_from_port
+    to_port     = var.egress_to_port
+    protocol    = var.egress_protocol
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = {
-    Name = "ecommerce_rds"
-  }
+  tags = var.aws_security_group_tag_name
 }
 
 resource "aws_db_parameter_group" "ecommerce" {
-  name   = "ecommerce"
-  family = "mysql8.0"
+  name   = var.name
+  family = var.family
 
   parameter {
-    name  = "character_set_server"
-    value = "utf8"
+    name  = var.parameter_group_name
+    value = var.parameter_group_value
   }
 }
 
 resource "aws_db_instance" "ecommerce" {
-  identifier             = "ecommerce"
-  instance_class         = "db.t3.micro"
-  allocated_storage      = 20
-  storage_type           = "gp2"
-  engine                 = "mysql"
-  engine_version         = "8.0.28"
-  multi_az               = true
-  username               = "admin"
+  identifier             = var.identifier
+  instance_class         = var.instance_class
+  allocated_storage      = var.allocated_storage
+  backup_retention_period = var.backup_retention_period
+  max_allocated_storage  = var.max_allocated_storage
+  storage_type           = var.storage_type
+  engine                 = var.engine
+  engine_version         = var.engine_version
+  multi_az               = var.multi_az
+  username               = var.username
   password               = var.db_password
   db_subnet_group_name   = aws_db_subnet_group.ecommerce.name
   vpc_security_group_ids = [aws_security_group.rds.id]
   parameter_group_name   = aws_db_parameter_group.ecommerce.name
-  publicly_accessible    = false
-  skip_final_snapshot    = true
+  publicly_accessible    = var.publicly_accessible
+  skip_final_snapshot    = var.skip_final_snapshot
+  final_snapshot_identifier = var.final_snapshot_identifier
 }
